f1df572f034c36d1677d9a3228036234a0ce62b9
nouhajbeli <70015527+nouhajbeli@users.noreply.github.com>
2021-01-06 05:28:13 -0800

    up

---
+++ front/src/app/app-routing.module.ts
@@ -0,0 +1,55 @@
+import { AuthGuard } from './guards/auth.guard';
+import { PaypalComponent } from './components/paypal/paypal.component';
+import { LibraryComponent } from './components/library/library.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupInstructorComponent } from './components/signup-instructor/signup-instructor.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupStudentComponent } from './components/signup-student/signup-student.component';
+import { HomeUserComponent } from './components/home-user/home-user.component';
+import { StoreComponent } from './components/store/store.component';
+import { NewcourseComponent } from './components/newcourse/newcourse.component';
+import { StudentGuard } from './guards/student.guard';
+import { InstructorGuard } from './guards/instructor.guard';
+import { HomeGuard } from './guards/home.guard';
+import {ResultComponent} from './components/result/result.component';
+import { EditComponent } from './components/edit/edit.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [HomeGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [HomeGuard] },
+  { path: 'coursedetails/:id', component: CourseDetailsComponent },
+  {
+    path: 'signupstudent',
+    component: SignupStudentComponent,
+    canActivate: [HomeGuard],
+  },
+  {
+    path: 'signupinstructor',
+    component: SignupInstructorComponent,
+    canActivate: [HomeGuard],
+  },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeUserComponent },
+  { path: 'store', component: StoreComponent, canActivate: [InstructorGuard] },
+  {
+    path: 'newcourse',
+    component: NewcourseComponent,
+    canActivate: [InstructorGuard],
+  },
+  { path: 'library', component: LibraryComponent, canActivate: [StudentGuard] },
+  { path: 'paypal', component: PaypalComponent },
+  { path:'result/:query' , component:  ResultComponent},
+  { path:'edit/:id' , component:  EditComponent}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ server/server/routers/courseRouter.js
@@ -0,0 +1,196 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+var Course = require("../../database/index");
+var { CourseModel } = require("../../database/models/courseModel");
+const multer = require("multer");
+
+const DIR = "../front/src/assets/uploads/courses/";
+var name_file;
+var videofile;
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, DIR);
+  },
+  filename: (req, file, cb) => {
+    const fileName = file.originalname.toLowerCase().split(" ").join("-");
+    name_file = fileName;
+    //videofile = videoName;
+    cb(null, fileName);
+  }
+});
+
+// Multer Mime Type Validation
+var upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 100024 * 1024 * 5
+  },
+  fileFilter: (req, file, cb) => {
+    cb(null, true);
+  }
+});
+
+router.route("/api/newCourse").post(upload.array("file", 2), (req, res) => {
+  const reqFiles = [];
+  const url = req.protocol + "://" + req.get("host");
+  for (var i = 0; i < req.files.length; i++) {
+    reqFiles.push(req.files[i].filename);
+  }
+  console.log(req.files);
+  const product = new CourseModel({
+    _id: new mongoose.Types.ObjectId(),
+    IdInstructor: req.body.IdInstructor,
+    title: req.body.title,
+    description: req.body.description,
+    video: reqFiles[0],
+    pdf: reqFiles[1],
+    category: req.body.category,
+    type: req.body.type,
+    price: req.body.price
+  });
+
+  Course.addCourse(product)
+    .then((data, err) => {
+      if (err) res.send(err);
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.route("/api/allcourses").get(function (req, res) {
+  Course
+    .findCourses()
+    .then((courses, err) => {
+      if (err) res.send(err);
+      res.send(courses);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.route("/api/course/:id").delete(function (req, res) {
+  Course
+    .deleteCourse(req.params.id)
+    .then((course, err) => {
+      if (err) res.send(err);
+      res.send(course);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.route("/api/course/:id").get(function (req, res) {
+  Course.findCourseById(req.params.id).then((data) => {
+    res.send(data);
+  });
+});
+
+
+router.route("/api/course/comment/:id").patch((req, res) => {
+  Course.courseComment(req.params.id,req.body)
+  .then((data)=>{
+    res.send(data)
+  })
+})
+router.route("/api/course/edit/:id").patch((req, res) => {
+  Course.editcommentCourse(req.params.id)
+  .then((data)=>{
+    console.log(data)
+    const thecomment=data.comments.find((comment)=>{
+      console.log(comment._id)
+      console.log(req.body)
+       return comment._id.equals(req.body.commentId)})
+     
+   if(!thecomment) return res.status(404).send('comment not found')
+    thecomment.text=req.body.text
+    return  data.save((err)=>{
+      console.log(data)
+      if(!err) return res.status(200).send(data)
+      return res.status(500).send(err)
+    
+    }) 
+  })
+  .catch((err)=>{
+    return res.status(400).send(err)
+  })
+})
+router.route("/api/course/delete-comment/:id").patch((req, res)=>{
+  Course.deletecommentCourse(req.params.id,req.body)
+  .then((data,err)=>{
+    if(!err) return res.send(data) 
+    else return res.status(400).send(err)  
+  })
+  .catch((err)=>{
+    return res.status(400).send(err)
+  })
+})
+router.route("/api/course/rate/:id").patch((req, res) => {
+  Course.courserate(req.params.id,req.body)
+  .then((data)=>{
+    res.send(data)
+  })
+})
+router.route("/api/course/edit-rate/:id").patch((req, res) => {
+  Course.editrateCourse(req.params.id)
+  .then((data)=>{
+    console.log(data)
+    const therate=data.rates.find((rate)=>{
+      console.log(rate._id)
+      console.log(req.body)
+       return rate._id.equals(req.body.rateId)})
+     
+   if(!therate) return res.status(404).send(' not found')
+    therate.rates=req.body.rates
+    return  data.save((err)=>{
+      console.log(data)
+      if(!err) return res.status(200).send(data)
+      return res.status(500).send(err)
+    
+    }) 
+  })
+  .catch((err)=>{
+    return res.status(400).send(err)
+  })
+})
+
+router.route("/api/update/course/:id").put(upload.array('file',2), (req, res) => {
+  const reqFiles = []
+  const url = req.protocol + '://' + req.get('host')
+  for (var i = 0; i < req.files.length; i++) {
+    reqFiles.push( req.files[i].filename)
+  }
+  console.log(req.files)
+  console.log(req.params.id)
+  const product = {
+    IdInstructor: req.body.IdInstructor,
+    title: req.body.title,
+    description: req.body.description,
+    video: reqFiles[0],
+    pdf: reqFiles[1],
+    category: req.body.category,
+    type: req.body.type,
+    price: req.body.price,
+    
+    
+  };
+
+  Course.updateCourse(req.params.id,product)
+    .then((data, err) => {
+      if (err) res.send(err);
+      console.log(data);
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+
+
+module.exports = router;
