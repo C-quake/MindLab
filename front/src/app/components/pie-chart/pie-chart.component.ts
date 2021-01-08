6cf93354366f4ea752de232b4213a0b7594f710f
Hbib24 <habib.bekir@gmail.com>
2021-01-08 10:10:33 +0100

    admin stats

--- front/src/app/components/pie-chart/pie-chart.component.ts
+++ front/src/app/components/pie-chart/pie-chart.component.ts
@@ -52,7 +52,7 @@ export class PieChartComponent {
     return this.instructorService.getAllInstructors();
   }
   allstudent() {
-    return this.studentService.findStudents();
+    return this.studentService.getAllStudents();
   }
   allcouses() {
     return this.storeservice.getService();
