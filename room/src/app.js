74bd79d46f53db913d5e832d3a644eb718794ff7
nouhajbeli <70015527+nouhajbeli@users.noreply.github.com>
2021-01-15 05:35:15 -0800

    up7

--- room/src/app.js
+++ room/src/app.js
@@ -10,6 +10,9 @@ app.use("/assets", express.static(path.join(__dirname, "assets")));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+app.get("/share", (req, res) => {
+  res.sendFile(__dirname + "/share.html");
+});
 
 io.of("/stream").on("connection", stream);
 
